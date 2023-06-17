a50ab074aa3e1478e63bf489b15a529d946a31ae
MrVauxs <mrvauxs@gmail.com>
2023-06-17 15:36:44 +0200

    update

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,34 @@
-<script lang='ts'>
-	// The ordering of these imports is critical to your app working properly
+<script lang="ts">
 	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
-	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
 	import '@skeletonlabs/skeleton/styles/skeleton.css';
-	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="header">
+		<div class="bg-surface-100-800-token shadow-xl">
+			<AppBar class="container">
+				<svelte:fragment slot="lead">
+					<div class="flex items-center">
+						<button class="btn btn-sm contents select-none">
+							<img src="/sign.svg" alt="Strefa Piesza" class="h-8 icon" draggable={false} />
+							<span>Strefa Piesza</span>
+						</button>
+					</div>
+				</svelte:fragment>
+			</AppBar>
+		</div>
+	</svelte:fragment>
+
+	<slot />
+</AppShell>
+
+<style>
+	.icon {
+		transform: scale(1.75);
+		padding: 0 1rem 0 0;
+		filter: drop-shadow(1px 1px 2px rgb(0 0 0 / 0.4));
+	}
+</style>
